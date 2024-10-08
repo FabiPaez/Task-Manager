9132d641185b2ba906d001c296bfd3a139db29ad
FabiPaez <89799622+FabiPaez@users.noreply.github.com>
2024-10-08 18:44:30 -0300

    Agregando configuración de despliegue

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,11 +1,21 @@
-const config = {   
-    user: 'task', 
-    password: 'Manager-123', // Cambia a la contraseña correcta
-    server: 'localhost', 
-    database: 'TaskManagerDB',
+// const config = {   
+//     user: 'task', 
+//     password: 'Manager-123', // Cambia a la contraseña correcta
+//     server: 'localhost', 
+//     database: 'TaskManagerDB',
+//     options: {
+//         port: 1434,
+//         trustServerCertificate: true
+//     }
+// };
+const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER, 
+    port: parseInt(process.env.DB_PORT, 10),
     options: {
-        port: 1434,
-        trustServerCertificate: true
+        encrypt: true, // Railway uses encrypted connections
+        trustServerCertificate: false // Cambia a true si es necesario
     }
 };
 
